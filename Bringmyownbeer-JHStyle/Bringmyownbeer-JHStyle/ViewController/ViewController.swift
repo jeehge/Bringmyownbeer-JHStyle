f1633d1b8dbcfa937700290e633811a24efc4597
JH <taerg89@gmail.com>
2021-11-21 20:43:50 +0900

    Create Project

---
+++ Bringmyownbeer-JHStyle/Bringmyownbeer-JHStyle/ViewController/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Bringmyownbeer-JHStyle
+//
+//  Created by JH on 2021/11/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
