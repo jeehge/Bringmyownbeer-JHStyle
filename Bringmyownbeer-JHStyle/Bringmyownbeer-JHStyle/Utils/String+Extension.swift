902a62f2e31aca712baaf3aed4c9d3a65264ef01
JH <taerg89@gmail.com>
2021-11-25 00:19:02 +0900

    Add Celldentifierable, String Extension

--- Bringmyownbeer-JHStyle/Bringmyownbeer-JHStyle/Utils/String+Extension.swift
+++ Bringmyownbeer-JHStyle/Bringmyownbeer-JHStyle/Utils/String+Extension.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+extension String {
+    // 다국어화
+    var localized: String {
+        return NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: "", comment: "")
+    }
+
+    // 글자 치환
+    func replace(_ target: String, _ withString: String) -> String {
+        return self.replacingOccurrences(of: target, with: withString, options: NSString.CompareOptions.literal, range: nil)
+    }
+}
