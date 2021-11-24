98c946cc4d5e14b8c2efa5963212b68014c392f4
JH <taerg89@gmail.com>
2021-11-25 00:19:59 +0900

    Add BeerListCell

--- Bringmyownbeer-JHStyle/Bringmyownbeer-JHStyle/ViewController/BeerListCell.swift
+++ Bringmyownbeer-JHStyle/Bringmyownbeer-JHStyle/ViewController/BeerListCell.swift
@@ -5,4 +5,46 @@
 //  Created by JH on 2021/11/24.
 //
 
-import Foundation
+import UIKit
+
+final class BeerListCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "test"
+        label.textColor = .gray
+        return label
+    }()
+    
+    // MARK: - Initialize
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setConstraint()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setConstraint()
+    }
+    
+    // MARK: - private Method
+    
+    private func setConstraint() {
+        
+    }
+    
+    // MARK: - internal Method
+    
+    func setBeerListCell() {
+
+    }
+}
