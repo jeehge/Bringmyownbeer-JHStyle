d8b393641a2d686f4de8548f7ac1789c7f24a71e
JH <taerg89@gmail.com>
2021-11-25 00:20:26 +0900

    made TableView

--- Bringmyownbeer-JHStyle/Bringmyownbeer-JHStyle/ViewController/ListViewController.swift
+++ Bringmyownbeer-JHStyle/Bringmyownbeer-JHStyle/ViewController/ListViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class ListViewController: BaseViewController {
 
-    // tableView 생성
+    // MARK: - Properties
     private let tableView: UITableView = {
         let tableview = UITableView()
         return tableview
@@ -18,9 +18,44 @@ final class ListViewController: BaseViewController {
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configTableView()
     }
+    
+    private func configTableView() {
+//        tableView.delegate = self
+        tableView.dataSource = self
 
+        tableView.register(BeerListCell.self, forCellReuseIdentifier: BeerListCell.identifier)
+        
+        setConstraint()
+        
+        // autoHeight
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+    }
+    
+    private func setConstraint() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
 
+        NSLayoutConstraint.activate([
+           tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+           tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+           tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+           tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        ])
+    }
 }
 
+// MARK: - UITableViewDataSource
+extension ListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: BeerListCell.identifier, for: indexPath) as? BeerListCell else { return UITableViewCell()}
+        return cell
+    }
+}
