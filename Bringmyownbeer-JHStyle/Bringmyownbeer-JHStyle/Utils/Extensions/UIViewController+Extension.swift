96a0021a71287ca855e6111687c75490fd5c82a8
JH <taerg89@gmail.com>
2021-11-27 00:41:03 +0900

    Add UIViewController+Extension

--- Bringmyownbeer-JHStyle/Bringmyownbeer-JHStyle/Utils/Extensions/UIViewController+Extension.swift
+++ Bringmyownbeer-JHStyle/Bringmyownbeer-JHStyle/Utils/Extensions/UIViewController+Extension.swift
@@ -5,4 +5,14 @@
 //  Created by JH on 2021/11/26.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func push(_ viewController: UIViewController, animated: Bool = true) {
+        navigationController?.pushViewController(viewController, animated: animated)
+    }
+
+    func pop(animated: Bool = true) {
+        navigationController?.popViewController(animated: animated)
+    }
+}
