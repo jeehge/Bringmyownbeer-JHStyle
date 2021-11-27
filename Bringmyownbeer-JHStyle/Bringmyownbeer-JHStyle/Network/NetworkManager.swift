c812ac197efb0178d7f64c6d395140ca8fde420c
JH <taerg89@gmail.com>
2021-11-27 11:06:27 +0900

    Modfiy NetworkManager

--- Bringmyownbeer-JHStyle/Bringmyownbeer-JHStyle/Network/NetworkManager.swift
+++ Bringmyownbeer-JHStyle/Bringmyownbeer-JHStyle/Network/NetworkManager.swift
@@ -8,19 +8,19 @@
 import Foundation
 
 final class NetworkManager {
-    static func request<T: Decodable>(api: API, parameters: [String: String]?, completion: @escaping (Result<[T], Error>) -> Void) {
+    static func request<T: Decodable>(api: API, parameters: [String: String]?, completion: @escaping (Result<[T], APIError>) -> Void) {
         guard let request: URLRequest = getFinalURL(api: api,
                                                     parameters: parameters) else { return }
         URLSession.shared.dataTask(with: request) {(data, response, error) in
             DispatchQueue.main.async {
                 if let error = error {
-                    completion(.failure(error))
+                    completion(.failure(.message(error.localizedDescription)))
                 }
                 
                 guard let response = response as? HTTPURLResponse else { return }
                 if (200..<300).contains(response.statusCode){
                     guard let data = data else {
-                        completion(.failure(APIError.noData))
+                        completion(.failure(.noData))
                         return
                     }
                     
@@ -28,7 +28,7 @@ final class NetworkManager {
                         let result = try JSONDecoder().decode([T].self, from: data)
                         completion(.success(result))
                     } catch {
-                        completion(.failure(APIError.parseError))
+                        completion(.failure(.parseError))
                         return
                     }
                 } else {
@@ -43,7 +43,6 @@ final class NetworkManager {
         if let param = parameters {
             components.setQueryItems(with: param)
         }
-
         
         guard let url = components.url else { return nil }
         var request = URLRequest(url: url)
